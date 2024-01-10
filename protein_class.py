489cb20b7be878a4f0221b4cf80731e173f94005
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-10 15:39:37 +0100

    Added grid

--- protein_class.py
+++ protein_class.py
@@ -1,6 +1,7 @@
 class Protein:
     def __init__(self, string):
-        self._length = len(string)
+        self._length: int = len(string)
+        self._grid: dict[tuple, "Aminoacid"] = dict()
 
     def get_length(self) -> int:
         return self._length
