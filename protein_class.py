f60b91aec83979931bc67e5553e5f61cca045e08
Ilyass el Allali <illiyas5612@outlook.com>
2024-01-10 16:54:47 +0100

    Added example of the 'double linked list'.

--- protein_class.py
+++ protein_class.py
@@ -19,20 +19,36 @@ class Protein:
             new_aminoacid = Aminoacid(predecessor=current, link=None, type=type)
             current._link = new_aminoacid
             current = new_aminoacid
-        
+
         return head
 
     def get_score(self):
         return self._score
     
 
+
+
 # Example usage:
 protein_sequence = "ABCDEF"
 protein = Protein(protein_sequence)
-
 current_node = protein._head
+
+print("From left to right: \n")
 while current_node:
     print(current_node._char, end=" ")
-    current_node = current_node._link
-    previous_node = current_node._predecessor
-    print(previous_node._char, end=" ")
+    if current_node._link == None:
+        print("\n\n")
+        break
+    else:
+        current_node = current_node._link
+
+print("From right to left: \n")
+while current_node:
+    print(current_node._char, end=" ")
+    if current_node._predecessor._char == "A":
+        current_node = current_node._predecessor
+        print(current_node._char, end=" ")
+        print("\n")
+        break
+    else:
+        current_node = current_node._predecessor
