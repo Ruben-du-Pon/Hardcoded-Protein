6b97b7b746e9e29b0d5146cbb15c814d89395274
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-10 15:14:10 +0100

    Split classes to different files

--- aminoacid_class.py
+++ aminoacid_class.py
@@ -4,25 +4,13 @@ class Aminoacid:
         self._position: tuple[int] = (0, 0)
         self._type: str = type
 
-    def add_connection(self, other: "Aminoacid") -> None:
-        self._connections.add(other)
-
-    def check_connection(self, other: "Aminoacid") -> bool:
-        return other in self._connections
-
-    def is_adjacent(self, other: "Aminoacid") -> bool:
-        return abs(self.sum_position - other.sum_position) == 1
-
     def get_position(self) -> tuple[int]:
         return self._position
 
     def set_position(self, position: tuple) -> None:
         self._position = position
 
-    def sum_position(self) -> tuple:
-        return self._position[1] + self._position[2]
-
-    def find_type(self) -> str:
+    def get_type(self) -> str:
         return self._type
 
     def type_score(self, other: "Aminoacid") -> int:
@@ -34,9 +22,3 @@ class Aminoacid:
 
         if (self.find_type == "C"):
             return -5
-
-
-class Grid:
-
-    def __init__(self):
-        pass
---
+++ protein_class.py
@@ -0,0 +1,3 @@
+class Protein:
+    def __init__(self, string):
+        self._length = len(string)
