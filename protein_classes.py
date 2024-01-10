6d86303d2d7128cae49719b8a45f84a935417d4c
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-10 13:59:31 +0100

    Changed check_position and added sum_position

--- protein_classes.py
+++ protein_classes.py
@@ -1,13 +1,16 @@
 class Aminoacid:
 
     def __init__(self):
-        self._position = 0
+        self._position = (0, 0)
 
-    def check_position(self, other):
-        return abs(self._position - other._position)
+    def check_position(self, other: "Aminoacid") -> int:
+        return abs(self.sum_position - other.sum_position)
 
-    def set_position(self, position):
+    def set_position(self, position: tuple) -> None:
         self._position = position
+
+    def sum_position(self) -> tuple:
+        return self._position[1] + self._position[2]
 
 
 class Protein:
