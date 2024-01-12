8d5b9cd94068d029a968a75c4edb9ccc7c10428b
Siard Frints <siard123@live.nl>
2024-01-12 14:38:14 +0100

    added optional arg grid for visual

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
+import csv, sys
 from code.classes import protein
 
 def main():
+    showGrid = True if len(sys.argv) == 2 and sys.argv[1] == "grid" else False
+
     with open("data/input/sequences_H_P.csv", "r") as file:
         reader = csv.reader(file)
         line_number = 0
@@ -13,7 +15,10 @@ def main():
             sequence= row[0]
             test_protein=protein.Protein(sequence)
             test_protein.create_csv(line_number)
-            print(test_protein.get_grid_2D())
+
+            if showGrid:
+                print(test_protein.get_grid_2D())
+
             line_number += 1
             
 if __name__ == "__main__":
