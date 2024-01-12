be56df7785748c211e7c5cd5c6d3ebdbd8b4822d
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-12 12:29:35 +0100

    main.py geeft output

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import csv
 from code.classes import protein
 
-sequence = "HHPHHHPH"
-test_protein = protein.Protein(sequence)
-test_protein.create_csv()
+def main():
+    with open("data/input/sequences_H_P.csv", "r") as file:
+        reader = csv.reader(file)
+        line_number = 0
+        
+        for row in reader:
+            if not row:
+                break
+            
+            sequence= row[0]
+            test_protein=protein.Protein(sequence)
+            test_protein.create_csv(line_number)
+            line_number += 1
+            
+if __name__ == "__main__":
+    main()
