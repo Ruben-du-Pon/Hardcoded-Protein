e88f2bf60e60b43378cd855e3dbbd427d9e36b0c
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-13 14:51:28 +0100

    Removed obsolete methods

--- code/classes/protein.py
+++ code/classes/protein.py
@@ -41,7 +41,7 @@ class Protein:
     def get_score(self):
         return self._score
 
-    def get_folding(self):
+    def get_folding(self) -> List[Dict[str, int]]:
         folding = []
         current = self._head
 
@@ -78,121 +78,13 @@ class Protein:
 
         filename = "data/output/output" + str(index) + ".csv"
         with open(filename, 'w', newline='') as file:
-            print(filename)
+            print(f"{filename} created.")
             header = ["amino", "fold"]
             writer = csv.DictWriter(file, fieldnames=header)
             folding = self.get_folding()
 
             writer.writeheader()
             writer.writerows(folding)
-
-    def generate_random_protein_positions(self, backtracking=False):
-        """
-        Generates a random, valid sequence of positions for the amino acids in the protein.
-
-        It uses backtracking to avoid overlapping positions, ensuring a valid 3D structure. 
-        Raises ValueError if a valid structure cannot be generated.
-
-        Returns a list of 3D coordinates for each amino acid.
-        """  # noqa
-
-        directions = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
-        positions = [(0, 0, 0)]  # Starting position
-
-        if backtracking:
-            self._grid[positions[0]] = self._sequence[0]
-
-            if not self._place_next_amino_acid(positions, 1, directions):
-                raise ValueError(
-                    "Cannot generate a valid protein structure with the given sequence.")  # noqa
-
-            return positions
-
-        for _ in range(1, self._length):
-            last_position = positions[-1]
-            movement = random.choice(directions)
-            next_position = (tuple(map(add, last_position, movement)))
-            positions.append(next_position)
-
-        return positions
-
-    def _place_next_amino_acid(self, positions, index, directions):
-        """
-        Recursive method to place an amino acid in a valid position.
-
-        Args:
-        positions: List of current amino acid positions.
-        index: Index of the amino acid to be placed.
-        directions: Possible directions to move from the current position.
-
-        Returns a bool: True if a valid position is found, False otherwise (backtracks if False).
-        """  # noqa
-
-        if index >= self._length:
-            return True
-
-        last_position = positions[-1]
-        random.shuffle(directions)  # Randomize the directions to try
-
-        for movement in directions:
-            next_position = tuple(map(add, last_position, movement))
-
-            if self.is_valid_fold(next_position):
-                positions.append(next_position)
-                self._grid[next_position] = self._sequence[index]
-
-                if self._place_next_amino_acid(positions, index + 1, directions):  # noqa
-                    return True
-
-                # Backtrack
-                positions.pop()
-                del self._grid[next_position]
-
-        return False
-
-    def get_grid_2D(self) -> str:
-        """
-        Generates a 2D grid representation of the protein structure.
-
-        Returns a string representation of the 2D grid, showing amino acids
-        and their connections.
-        """
-
-        sequence = []
-        current = self._head
-        while current:
-            sequence.append(current.get_type())
-            current = current.link
-
-        # Determine the grid size
-        min_x = min(x for x, _, _ in self._positions)
-        max_x = max(x for x, _, _ in self._positions)
-        min_y = min(y for _, y, _ in self._positions)
-        max_y = max(y for _, y, _ in self._positions)
-        grid_size_x = (max_x - min_x) * 2 + 3
-        grid_size_y = (max_y - min_y) * 2 + 3
-
-        # Create an empty grid
-        grid = [[' ' for _ in range(grid_size_x)] for _ in range(grid_size_y)]
-
-        # Adjust positions for the grid
-        adjusted_positions = [(2 * (x - min_x) + 1, 2 * (y - min_y) + 1)
-                              for x, y, _ in self._positions]
-
-        # Place amino acids and connections
-        for (x, y), amino_acid in zip(adjusted_positions, sequence):
-            grid[y][x] = '' + amino_acid + ''
-        for (x1, y1), (x2, y2) in zip(adjusted_positions, adjusted_positions[1:]):  # noqa
-            if x1 == x2:
-                for y in range(min(y1, y2) + 1, max(y1, y2)):
-                    grid[y][x1] = '|'
-            elif y1 == y2:
-                for x in range(min(x1, x2) + 1, max(x1, x2)):
-                    grid[y1][x] = '-'
-
-        # Convert the grid to a string representation
-        grid_str = '\n'.join([''.join(row) for row in grid])
-        return grid_str
 
     def is_valid(self) -> bool:
         """
