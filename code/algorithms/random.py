4a09668d1a48a7b91aaa2f7e88f6549364228dab
Ruben-du-Pon <151724616+Ruben-du-Pon@users.noreply.github.com>
2024-01-16 20:34:43 +0100

    Added implementation for 2D or 3D folding to RandomSearch

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -3,31 +3,39 @@ from typing import Optional
 from ..classes.protein import Protein
 from ..classes.aminoacid import Aminoacid
 
-DIRECTIONS = [(1, 0, 0), (-1, 0, 0), (0, 1, 0),
+DIRECTIONS_2D = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
+
+DIRECTIONS_3D = [(1, 0, 0), (-1, 0, 0), (0, 1, 0),
               (0, -1, 0), (0, 0, 1), (0, 0, -1)]
 
 
 class RandomSearch:
 
-    def __init__(self, protein: Protein, cross_allowed: Optional[bool] = True) -> None:
+    def __init__(self, protein: Protein, dimensions: int, no_crossing: Optional[bool] = False) -> None:
         self._protein = protein
         self._positions = {(0, 0, 0)}
-        self._cross_allowed: cross_allowed
+        self._no_crossing: no_crossing
+        self._dimensions = dimensions
 
     def fold_protein(self) -> None:
         current = self._protein.get_list().link
         while current.link is not None:
-            if self._cross_allowed:
-                self.set_position(current)
-                current = current.link
-            else:
+            if self._no_crossing:
                 while current.position in self._positions:
                     self.set_position(current)
                 self._positions.add(current.position)
                 current = current.link
+            else:
+                self.set_position(current)
+                current = current.link 
+                
 
     def get_random_direction(self) -> tuple[int, int, int]:
-        return random.choice(DIRECTIONS)
+        if self._dimensions == 2:
+          directions = DIRECTIONS_2D 
+        elif self._dimensions == 3:
+          directions = DIRECTIONS_3D 
+        return random.choice(directions)
 
     def set_position(self, acid: Aminoacid):
         acid.position = tuple(
