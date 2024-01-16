2afdef756280b2073fe9dd4da8e4339320799c6b
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-16 23:19:59 +0100

    Renames class RandomFold

--- code/algorithms/random.py
+++ code/algorithms/random.py
@@ -1,24 +1,19 @@
-import random
 from typing import Optional, Tuple
 from ..classes.protein import Protein
 from ..classes.aminoacid import Aminoacid
-
-DIRECTIONS_2D = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
-
-DIRECTIONS_3D = [(1, 0, 0), (-1, 0, 0), (0, 1, 0),
-                 (0, -1, 0), (0, 0, 1), (0, 0, -1)]
+import random
 
 
-class RandomSearch:
+class RandomFold:
     """
-    Class for performing a random search on a protein sequence.
+    Class for performing a random folding on a protein sequence.
 
     Parameters
     ----------
     protein : Protein
-        The protein sequence on which the random search is performed.
+        The protein sequence on which the random folding is performed.
     dimensions : int
-        The number of dimensions for the search (2 for 2D, 3 for 3D).
+        The number of dimensions for the folding (2 for 2D, 3 for 3D).
     no_crossing : Optional[bool], optional
         If True, ensures that no two amino acids occupy the same position,
         by repeatedly selecting a new position until a unique one is found.
@@ -26,43 +21,50 @@ class RandomSearch:
     Attributes
     ----------
     _protein : Protein
-        The protein sequence on which the random search is performed.
-    _positions : set
-        A set containing the positions of amino acids.
+        The protein sequence on which the random folding is performed.
+    _dimensions : int
+        The number of dimensions for the folding (2 for 2D, 3 for 3D).
     _no_crossing : Optional[bool]
         If True, ensures that no two amino acids occupy the same position.
-    _dimensions : int
-        The number of dimensions for the search (2 for 2D, 3 for 3D).
-    """
+
+    Methods
+    -------
+    run() -> None:
+        Perform the random folding on the protein sequence.
+        If no_crossing is True, ensures that no two amino acids occupy the
+        same position by repeatedly selecting a new position until a unique one is found.
+
+    get_random_direction() -> Tuple[int, int, int]:
+        Get a random direction for the folding.
+        Returns a tuple representing the random direction.
+
+    set_position(acid: Aminoacid) -> None:
+        Set the position of an amino acid based on a random direction.
+        If no_crossing is True, ensures that no two amino acids occupy the same position.
+
+    """  # noqa
 
     def __init__(self, protein: Protein, dimensions: int,
                  no_crossing: Optional[bool] = False) -> None:
         self._protein = protein
-        self._positions: set[Tuple[int, int, int]] = {(0, 0, 0)}
-        self._no_crossing: Optional[bool] = no_crossing
-        self._dimensions: int = dimensions
+        self._dimensions = dimensions
+        self._no_crossing = no_crossing
 
     def run(self) -> None:
         """
-        Perform the random search on the protein sequence.
+        Perform the random folding on the protein sequence.
 
         If no_crossing is True, ensures that no two amino acids occupy the
         same position by repeatedly selecting a new position until a unique one is found.
-        """
+        """  # noqa
         current = self._protein.get_list().link
         while current.link is not None:
-            if self._no_crossing:
-                while current.position in self._positions:
-                    self.set_position(current)
-                self._positions.add(current.position)
-                current = current.link
-            else:
-                self.set_position(current)
-                current = current.link
+            self.set_position(current)
+            current = current.link
 
     def get_random_direction(self) -> Tuple[int, int, int]:
         """
-        Get a random direction for the search.
+        Get a random direction for the folding.
 
         Returns
         -------
@@ -70,20 +72,29 @@ class RandomSearch:
             A tuple representing the random direction.
         """
         if self._dimensions == 2:
-            directions = DIRECTIONS_2D
+            directions = [(1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0)]
         elif self._dimensions == 3:
-            directions = DIRECTIONS_3D
+            directions = [(1, 0, 0), (-1, 0, 0), (0, 1, 0),
+                          (0, -1, 0), (0, 0, 1), (0, 0, -1)]
         return random.choice(directions)
 
     def set_position(self, acid: Aminoacid) -> None:
         """
         Set the position of an amino acid based on a random direction.
 
+        If no_crossing is True, ensures that no two amino acids occupy the same position.
+
         Parameters
         ----------
         acid : Aminoacid
             The amino acid for which the position is set.
-        """
-        acid.position = tuple(
-            x + y + z for x, y, z in zip(acid.predecessor.position,
-                                         self.get_random_direction()))
+        """  # noqa
+        while True:
+            random_direction = self.get_random_direction()
+            new_position = tuple(
+                x + y for x, y in zip(acid.position, random_direction))
+
+            if not self._no_crossing or self._protein.is_valid_fold(new_position):
+                acid.position = new_position
+                self._protein.add_to_grid(new_position, acid)
+                break
