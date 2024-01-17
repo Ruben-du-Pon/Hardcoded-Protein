72d6d62e506fc75bf7304af08fb6ecba6eff3dde
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-17 10:51:26 +0100

    Adjusted spiral to be implemented as class

--- code/algorithms/spiral.py
+++ code/algorithms/spiral.py
@@ -2,67 +2,54 @@ from ..classes.protein import Protein
 from typing import List, Tuple
 
 
-def spiral_fold(protein: Protein) -> None:
-    """
-    Apply a spiral folding algorithm to the given protein.
+class SpiralFold:
+    def __init__(self, protein: Protein, dimensions: int) -> None:
+        """
+        Initialize the SpiralFold object.
 
-    The spiral folding algorithm starts from the second amino acid in the protein sequence,
-    and it adds each amino acid to a grid in a spiral pattern. The algorithm continues until
-    every amino acid in the protein has a different position on the grid.
+        Parameters
+        ----------
+        protein : Protein
+            The protein structure to which the spiral folding algorithm is applied.
+        dimensions : int
+            Represents the dimensions of folding (2 or 3).
+        """
+        self._protein = protein
+        self._dimensions = dimensions
+        self._movements: List[Tuple[int, int, int]] = [
+            (0, 1, 0), (1, 0, 0), (0, -1, 0), (-1, 0, 0)
+        ]
+        self._steps: int = 1
 
-    Parameters
-    ----------
-    protein : Protein
-        The protein structure to which the folding algorithm is applied.
+    def run(self) -> None:
+        """
+        Apply a spiral folding algorithm to the given protein.
 
-    Raises
-    ------
-    ValueError
-        If a valid folding cannot be found for the given protein.
+        Raises
+        ------
+        ValueError
+            If a valid folding cannot be found for the given protein.
+        """
+        current = self._protein.get_list().link
+        self._protein.add_to_grid(
+            current.predecessor.position, current.predecessor)
+        self._protein.add_to_grid(current.position, current)
+        movement_index: int = 0
 
-    Notes
-    -----
-    The function assumes that the protein structure has a double-linked list representation,
-    and the positions of amino acids are stored as tuples of three integers.
+        while not self._protein.is_valid():
 
-    Example
-    -------
-    protein_sequence = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
-    my_protein = Protein(protein_sequence)
-    spiral_fold(my_protein)
-    """
+            for _ in range(self._steps):
+                current.position = tuple(
+                    sum(x) for x in zip(current.predecessor.position, self._movements[movement_index])
+                )
+                self._protein.add_to_grid(current.position, current)
+                if current.link is None:
+                    break
+                current = current.link
 
-    current = protein.get_list().link
-    protein.add_to_grid(current.position, current)
+            movement_index = (movement_index + 1) % len(self._movements)
+            self._steps += 1 if movement_index % 2 == 0 else 0
 
-    movements: List[Tuple[int, int, int]] = [
-        (0, 1, 0), (1, 0, 0), (0, -1, 0), (-1, 0, 0)]
-    movement_index: int = 0
-    # Number of steps in the current direction
-    steps: int = 1
-
-    while not protein.is_valid():
-
-        # Move in the current direction for the specified number of steps
-        for _ in range(steps):
-
-            # Set position to the sum of the current position and the movement vector
-            current.position = tuple(
-                sum(x) for x in zip(current.predecessor.position, movements[movement_index])
-            )
-            protein.add_to_grid(current.position, current)
-            if current.link is None:
-                break
-
-            current = current.link
-
-        # Increment movement_index and cycle back to 0 if it exceeds the list length
-        movement_index = (movement_index + 1) % len(movements)
-
-        # Increase the number of steps every two directions
-        if movement_index % 2 == 0:
-            steps += 1
-
-    if not protein.is_valid():
-        raise ValueError(
-            f"Couldn't find a valid folding for protein {protein}")
+        if not self._protein.is_valid():
+            raise ValueError(
+                f"Couldn't find a valid folding for protein {self._protein}")
