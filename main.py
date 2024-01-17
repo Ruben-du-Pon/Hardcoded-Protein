5b4f2f8b3e7dfde424f78de19d6e14daa9e96eb5
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-17 11:07:26 +0100

    Adjusted main.py to work with algorithms as classes instead.

--- main.py
+++ main.py
@@ -15,11 +15,13 @@ ALGORITHM_FILES = [
 
 def main() -> None:
     """
-    Main function to execute protein folding based on the specified algorithm and visualize the results.
+    Main function to execute protein folding based on the specified algorithm
+    and visualize the results.
 
-    The function reads the command line arguments to determine the fold algorithm, dimensions, and
-    whether the protein sequence includes type 'C'. It then dynamically imports the specified algorithm,
-    applies it to protein sequences from a CSV file, and visualizes the results in 2D or 3D.
+    The function reads the command line arguments to determine the fold
+    algorithm, dimensions, and whether the protein sequence includes type 'C'.
+    It then dynamically imports the specified algorithm, applies it to protein
+    sequences from a CSV file, and visualizes the results in 2D or 3D.
 
     Parameters
     ----------
@@ -36,9 +38,10 @@ def main() -> None:
 
     Notes
     -----
-    The function assumes that the protein sequences are stored in CSV files located in the "data/input/" directory.
-    The output CSV files and visualizations will be saved in the "data/output/csv/" and "data/output/plot/"
-    directories, respectively.
+    The function assumes that the protein sequences are stored in CSV files
+    located in the "data/input/" directory.
+    The output CSV files and visualizations will be saved in the "data/output/csv/"
+    and "data/output/plot/" directories, respectively.
     """
     if len(sys.argv) != 3 and len(sys.argv) != 4:
         print("Usage: python main.py <fold_algorithm> <dimensions> [C]")
@@ -50,9 +53,9 @@ def main() -> None:
         raise ValueError("Invalid fold type.")
 
     # Import the selected folding algorithm dynamically
-    fold_module = __import__(
-        f"code.algorithms.{fold_algorithm}", fromlist=[fold_algorithm])
-    fold_function = getattr(fold_module, f"{fold_algorithm}_fold")
+    fold_module = __import__(f"code.algorithms.{fold_algorithm}", fromlist=[
+                             f"{fold_algorithm}Fold"])
+    fold_class = getattr(fold_module, f"{fold_algorithm.capitalize()}Fold")
 
     if len(sys.argv) == 3:
         filename: str = "data/input/sequences_H_P.csv"
@@ -75,22 +78,27 @@ def main() -> None:
             sequence: str = row[0]
             test_protein: protein.Protein = protein.Protein(sequence)
 
-            # Call the selected folding algorithm
-            fold_function(test_protein)
+            # Initialize the folding algorithm class
+            fold_instance = fold_class(test_protein, int(sys.argv[2]))
 
-            test_protein.create_csv(fold_algorithm, line_number)
+            # Call the run method of the folding algorithm
+            fold_instance.run()
+
+            test_protein.create_csv(line_number, fold_algorithm)
             if sys.argv[2] == "2":
                 visualization_2D.plot_2d(
-                    test_protein, ("red", "blue", "green"), line_number, fold_algorithm)
+                    test_protein, ("red", "blue", "green"),
+                    line_number, fold_algorithm)
             elif sys.argv[2] == "3":
                 visualization_3D.plot_3d(
-                    test_protein, ("red", "blue", "green"), line_number, fold_algorithm)
+                    test_protein, ("red", "blue", "green"),
+                    line_number, fold_algorithm)
             else:
                 print("Please enter dimension as 2 or 3")
                 sys.exit(3)
 
             line_number += 1
-            # break is for test (1x plotten)
+            # break is for test (1x plotting)
             # break
 
 
