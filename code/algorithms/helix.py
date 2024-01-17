4ede2222fc939753cc2209110c15483ca808bceb
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-17 10:45:40 +0100

    Adjusted helix to be implemented as class

--- code/algorithms/helix.py
+++ code/algorithms/helix.py
@@ -2,66 +2,61 @@ from ..classes.protein import Protein
 from typing import List, Tuple
 
 
-def helix_fold(protein: Protein) -> None:
-    """
-    Apply a helical folding algorithm to the given protein.
+class HelixFold:
+    def __init__(self, protein: Protein, dimensions: int):
+        """
+        Initialize the HelixFold object.
 
-    The helical folding algorithm starts from the second amino acid in the protein sequence,
-    and it adds each amino acid to a grid in a helical pattern. The algorithm continues until
-    every amino acid in the protein has a different position on the grid.
+        Parameters
+        ----------
+        protein : Protein
+            The protein structure to which the helical folding algorithm is applied.
+        dimensions : int
+            Represents the dimensions of folding (2D or 3D).
+        """
 
-    Parameters
-    ----------
-    protein : Protein
-        The protein structure to which the helical folding algorithm is applied.
+        self._protein = protein
+        self._movements: List[Tuple[int, int, int]] = [
+            (0, 1, 0), (1, 0, 0), (0, -1, 0), (-1, 0, 0), (0, 0, 1)
+        ]
 
-    Raises
-    ------
-    ValueError
-        If a valid folding cannot be found for the given protein.
+    def run(self) -> None:
+        """
+        Apply a helical folding algorithm to the given protein.
 
-    Notes
-    -----
-    The function assumes that the protein structure has a double-linked list representation,
-    and the positions of amino acids are stored as tuples of three integers.
+        Raises
+        ------
+        ValueError
+            If a valid folding cannot be found for the given protein.
+        """
+        current = self._protein.get_list().link
+        self._protein.add_to_grid(
+            current.predecessor.position, current.predecessor)
+        self._protein.add_to_grid(current.position, current)
 
-    Example
-    -------
-    protein_sequence = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
-    my_protein = Protein(protein_sequence)
-    helix_fold(my_protein)
-    """
+        movement_index: int = 0
+        counter: int = 1
 
-    current = protein.get_list().link
-    protein.add_to_grid(current.position, current)
+        while not self._protein.is_valid():
+            movement = self._movements[movement_index]
+            if counter == 4:
+                movement = (0, 0, 1)
+                movement_index -= 1
+                counter = 0
 
-    movements: List[Tuple[int, int, int]] = [
-        (0, 1, 0), (1, 0, 0), (0, -1, 0), (-1, 0, 0)]
-    movement_index: int = 0
-    counter: int = 1
+            current.position = tuple(
+                sum(x) for x in zip(current.predecessor.position, movement)
+            )
+            self._protein.add_to_grid(current.position, current)
 
-    while not protein.is_valid():
-        movement = movements[movement_index]
-        if counter == 4:
-            movement = (0, 0, 1)
-            movement_index -= 1
-            counter = 0
-        # Set position to the sum of the current position and the movement vector
-        current.position = tuple(
-            sum(x) for x in zip(current.predecessor.position, movement)
-        )
-        protein.add_to_grid(current.position, current)
-        if current.link is None:
-            break
+            if current.link is None:
+                break
 
-        current = current.link
+            current = current.link
+            movement_index = (movement_index + 1) % len(self._movements)
+            counter += 1
 
-        # Increment movement_index and cycle back to 0 if it exceeds the list length
-        movement_index = (movement_index + 1) % len(movements)
-
-        # Increment the counter
-        counter += 1
-
-    if not protein.is_valid():
-        raise ValueError(
-            f"Couldn't find a valid folding for protein {protein}")
+        if not self._protein.is_valid():
+            raise ValueError(
+                f"Couldn't find a valid folding for protein {self._protein}"
+            )
