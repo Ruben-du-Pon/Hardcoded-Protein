4064563ab482bd714d0e7404173abd556ebb27b0
Ruben-du-Pon <rubenmdupon@gmail.com>
2024-01-31 17:44:36 +0100

    Updated main.py to use generation files

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import os
-import csv
 import sys
-from codefiles.classes.protein import Protein
-from codefiles.visualization import visualization_2D
-from codefiles.visualization import visualization_3D
+import subprocess
 
 # Dynamically get filenames without extensions from the algorithms directory
 ALGORITHM_FILES = [
@@ -15,115 +12,50 @@ ALGORITHM_FILES = [
 
 def main() -> None:
     """
-    Main function to execute protein folding based on the specified algorithm
-    and visualize the results.
+    Main function that reads protein sequences from a file and generates protein folds using the specified algorithm.
 
-    The function reads the command line arguments to determine the fold
-    algorithm, dimensions, whether the protein sequence includes type 'C', and
-    the output file format. It then dynamically imports the specified algorithm,
-    applies it to protein sequences from a CSV file, and visualizes the results in
-    2D or 3D.
+    Usage: python main.py <fold_algorithm> <dimensions> <iterations> [C/c]
 
-    Parameters
-    ----------
-    None
+    Parameters:
+    - fold_algorithm (str): The algorithm to use for protein folding.
+    - dimensions (int): The number of dimensions for the protein fold (2 or 3).
+    - iterations (int): The number of iterations for the protein folding process.
+    - C/c (optional): Specify 'C' or 'c' to use a different input file.
 
-    Returns
-    -------
-    None
-
-    Raises
-    ------
-    ValueError
-        If an invalid fold type is provided.
-
-    Notes
-    -----
-    The function assumes that the protein sequences are stored in CSV files
-    located in the "data/input/" directory.
-    The output files will be saved in the "data/output/csv/" and "data/output/plot/"
-    directories for CSV and visualization files, respectively.
+    Raises:
+    - ValueError: If the specified fold algorithm is invalid.
     """
-    filename: str = "data/input/sequences_H_P.csv"
 
-    if len(sys.argv) != 4 and len(sys.argv) != 5 and len(sys.argv) != 6:
+    # Check if the correct number of command line arguments is given
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
         print(
-            "Usage: python main.py <fold_algorithm> <dimensions> <iterations> [png/svg] [C/c]")
+            "Usage: python main.py <fold_algorithm> <dimensions> [C/c]")
         sys.exit(1)
 
     fold_algorithm: str = sys.argv[1].lower()
 
+    # Check if the specified dimensions are valid
     if sys.argv[2] not in ("2", "3"):
         print("Please enter dimension as 2 or 3")
         sys.exit(2)
 
     dimensions: int = int(sys.argv[2])
 
-    if sys.argv[3].isdigit() is False:
-        print("Please enter iterations as an integer")
-        sys.exit(3)
-
-    iterations: int = int(sys.argv[3])
-
     # Check if the specified fold algorithm is valid
     if fold_algorithm not in ALGORITHM_FILES:
         raise ValueError("Invalid fold type.")
 
-    # Import the selected folding algorithm dynamically
-    fold_module = __import__(f"codefiles.algorithms.{fold_algorithm}", fromlist=[
-                             f"{fold_algorithm}Fold"])
-    fold_class = getattr(fold_module, f"{fold_algorithm.capitalize()}Fold")
+    # Get a copy of the current environment variables
+    env = os.environ.copy()
+    # Add the parent directory of codefiles to the Python path in the new environment
+    env["PYTHONPATH"] = os.path.abspath(os.path.join(
+        os.path.dirname(__file__), '..')) + ":" + env.get("PYTHONPATH", "")
 
-    # Read the protein sequences from the CSV file
-    if len(sys.argv) == 4:
-        filename: str = "data/input/sequences_H_P.csv"
-
-    if len(sys.argv) == 5:
-        if sys.argv[4].lower() not in ("png", "svg"):
-            print("Please enter file format as png or svg")
-            sys.exit(5)
-
-    if len(sys.argv) == 6:
-        if sys.argv[5].lower() == "c":
-            filename: str = "data/input/sequences_H_P_C.csv"
-
-    with open(filename, "r") as file:
-        reader = csv.reader(file)
-        line_number: int = 0
-
-        # Read the file line by line until an empty line is reached
-        for row in reader:
-            if not row:
-                break
-
-            # Create Protein objects for each sequence
-            sequence: str = row[0]
-            test_protein: Protein = Protein(sequence)
-
-            # Initialize the folding algorithm class
-            fold_instance = fold_class(test_protein, dimensions, iterations)
-
-            # Call the run method of the folding algorithm
-            test_protein = fold_instance.run()
-
-            # Set the output filenames
-            if dimensions == 2:
-                filename = f"data/output/csv/{fold_algorithm}_{line_number}_2D.csv"
-                plotname = f"data/output/plot/{fold_algorithm}_{line_number}_2D.{sys.argv[4].lower() if (len(sys.argv) == 5 or len(sys.argv) == 6) and sys.argv[4] == 'png' else 'svg'}"
-            else:
-                filename = f"data/output/csv/{fold_algorithm}_{line_number}_3D.csv"
-                plotname = f"data/output/plot/{fold_algorithm}_{line_number}_3D.{sys.argv[4].lower() if (len(sys.argv) == 5 or len(sys.argv) == 6)  and sys.argv[4] == 'svg' else 'png'}"
-
-            # Write the results to a CSV file and the visualization to a file
-            test_protein.create_csv(filename, verbose=True)
-            if dimensions == 2:
-                visualization_2D.plot_2d(
-                    test_protein, ("red", "blue", "green"), plotname, sys.argv[4].lower() if (len(sys.argv) == 5 or len(sys.argv) == 6) and sys.argv[4] == 'png' else 'svg')
-            else:
-                visualization_3D.plot_3d(
-                    test_protein, ("red", "blue", "green"), plotname, sys.argv[4].lower() if (len(sys.argv) == 5 or len(sys.argv) == 6) and sys.argv[4] == 'png' else 'svg')
-
-            line_number += 1
+    # Call the generate_<algorithm>.py script with the correct parameters
+    subprocess.run(["python3",
+                    f"codefiles/fold_generation/generate_{fold_algorithm}.py",
+                    str(dimensions), "t" if len(sys.argv) == 4 else "f"],
+                   env=env)  # Pass the modified environment to the new process
 
 
 if __name__ == "__main__":
